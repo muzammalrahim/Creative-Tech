08978bd40bf9ea792606845e039cf19af5ecaa20
muzammal rahim <creative.joomdev@gmail.com>
2021-09-13 13:00:27 +0500

    build done

---
+++ creative-solutions/build/precache-manifest.28b6ea40a35d68c6badfe81803162a5b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b5b3a24a8d7f1d46161b82526fdc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268f49622ed8e30e525d",
+    "url": "/static/css/2.cbcf7c3a.chunk.css"
+  },
+  {
+    "revision": "5aaf1c5346bb97629fd3",
+    "url": "/static/css/main.641b687f.chunk.css"
+  },
+  {
+    "revision": "268f49622ed8e30e525d",
+    "url": "/static/js/2.65e3c8ef.chunk.js"
+  },
+  {
+    "revision": "a2fe7febc58e76ed718a8d9cdadb3d48",
+    "url": "/static/js/2.65e3c8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aaf1c5346bb97629fd3",
+    "url": "/static/js/main.094acaa3.chunk.js"
+  },
+  {
+    "revision": "edd564274ac933f7c4fc",
+    "url": "/static/js/runtime-main.1ee0b55e.js"
+  },
+  {
+    "revision": "02674537535001827ed67d83cf420aee",
+    "url": "/static/media/banner-bg.02674537.jpg"
+  },
+  {
+    "revision": "ba3d8c2a29499e3690b16b1e31c7c8bd",
+    "url": "/static/media/cta-bg-img.ba3d8c2a.jpg"
+  },
+  {
+    "revision": "b825316fd7164555f72a856725324808",
+    "url": "/static/media/slide-bg-1.b825316f.jpg"
+  },
+  {
+    "revision": "b825316fd7164555f72a856725324808",
+    "url": "/static/media/slide-bg-2.b825316f.jpg"
+  },
+  {
+    "revision": "b825316fd7164555f72a856725324808",
+    "url": "/static/media/slide-bg-3.b825316f.jpg"
+  },
+  {
+    "revision": "63b34bfbf7ce29258f7a15b4db130282",
+    "url": "/static/media/video-bg.63b34bfb.jpg"
+  }
+]);
